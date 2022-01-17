9e1930bc04992cd974e16ecdb8fbd47526887f46
Hrodvintir <33006079+Hrodvintir@users.noreply.github.com>
2022-01-17 18:26:24 +0200

    Create example.cpp

---
+++ week1/01 Practise Assignment/example.cpp
@@ -0,0 +1,85 @@
+#include "test_runner.h"
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <memory>
+#include <set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+class PriorityCollection {
+public:
+  using Id = 
+    
+  Id Add(T object);
+
+ 
+  template <typename ObjInputIt, typename IdOutputIt>
+  void Add(ObjInputIt range_begin, ObjInputIt range_end,
+           IdOutputIt ids_begin);
+
+  
+  bool IsValid(Id id) const;
+
+  
+  const T& Get(Id id) const;
+
+ 
+  void Promote(Id id);
+
+  
+  pair<const T&, int> GetMax() const;
+
+ 
+  pair<T, int> PopMax();
+
+private:
+  
+};
+
+
+class StringNonCopyable : public string {
+public:
+  using string::string;  
+  StringNonCopyable(const StringNonCopyable&) = delete;
+  StringNonCopyable(StringNonCopyable&&) = default;
+  StringNonCopyable& operator=(const StringNonCopyable&) = delete;
+  StringNonCopyable& operator=(StringNonCopyable&&) = default;
+};
+
+void TestNoCopy() {
+  PriorityCollection<StringNonCopyable> strings;
+  const auto white_id = strings.Add("white");
+  const auto yellow_id = strings.Add("yellow");
+  const auto red_id = strings.Add("red");
+
+  strings.Promote(yellow_id);
+  for (int i = 0; i < 2; ++i) {
+    strings.Promote(red_id);
+  }
+  strings.Promote(yellow_id);
+  {
+    const auto item = strings.PopMax();
+    ASSERT_EQUAL(item.first, "red");
+    ASSERT_EQUAL(item.second, 2);
+  }
+  {
+    const auto item = strings.PopMax();
+    ASSERT_EQUAL(item.first, "yellow");
+    ASSERT_EQUAL(item.second, 2);
+  }
+  {
+    const auto item = strings.PopMax();
+    ASSERT_EQUAL(item.first, "white");
+    ASSERT_EQUAL(item.second, 0);
+  }
+}
+
+int main() {
+  TestRunner tr;
+  RUN_TEST(tr, TestNoCopy);
+  return 0;
+}
