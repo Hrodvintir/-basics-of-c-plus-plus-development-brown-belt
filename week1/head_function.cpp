6b1dc9e42cb50cd472f777dc3e35ffd18f24653d
Hrodvintir <33006079+Hrodvintir@users.noreply.github.com>
2022-01-17 18:43:06 +0200

    Create head_function.cpp

---
+++ week1/head_function.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <set>
+#include <vector>
+using namespace std;
+
+template <typename Iterator>
+class IteratorRange {
+public:
+  IteratorRange(Iterator begin, Iterator end)
+    : first(begin)
+    , last(end)
+  {
+  }
+
+  Iterator begin() const {
+    return first;
+  }
+
+  Iterator end() const {
+    return last;
+  }
+
+private:
+  Iterator first, last;
+};
+
+template <typename Collection>
+auto Head(Collection& v, size_t top) {
+  return IteratorRange{v.begin(), next(v.begin(), min(top, v.size()))};
+}
