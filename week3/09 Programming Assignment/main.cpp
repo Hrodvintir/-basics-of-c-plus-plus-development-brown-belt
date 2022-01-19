07059459359a6230a4eac367314c83b2dbfef45c
Hrodvintir <33006079+Hrodvintir@users.noreply.github.com>
2022-01-19 10:47:35 +0200

    Create main.cpp

---
+++ week3/09 Programming Assignment/main.cpp
@@ -0,0 +1,27 @@
+#include "print_stats.h"
+#include "lazy_value.h"
+#include "synchronized.h"
+#include "concurrent_map.h"
+#include "polynomial.h"
+#include "object_pool.h"
+#include "animals.h"
+#include "pipeline.h"
+#include "Common.h"
+
+
+int main()
+{
+    /*
+     * TestPrintStats();
+	TestLazyValue();
+	TestSynchronized();
+	TestConcurrentMap();
+	TestPolynomial();
+	TestObjectPool();
+	TestZoo();
+	TestPipeLine();
+     */
+	TestCommon();
+
+	return 0;
+}
