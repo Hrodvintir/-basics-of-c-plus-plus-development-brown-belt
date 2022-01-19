4c729be1261da9edb3ad7076f1cce260ab26bee6
Hrodvintir <33006079+Hrodvintir@users.noreply.github.com>
2022-01-19 21:19:32 +0200

    Update main.cpp

--- week2/05 Programming Assingment/main.cpp
+++ week2/05 Programming Assingment/main.cpp
@@ -1,72 +1,57 @@
-#include "UnitTestsFramework.h"
+#include "test_runner.h"
 #include "stats_aggregator.h"
+
 #include <string>
 #include <memory>
 #include <iostream>
 #include <unordered_map>
 #include <functional>
-
-
 using namespace std;
-
 
 void TestAll();
 
+unique_ptr<StatsAggregator> ReadAggregators(istream& input) {
+  const unordered_map<string, std::function<unique_ptr<StatsAggregator>()>> known_builders = {
+    {"sum", [] { return make_unique<StatsAggregators::Sum>(); }},
+    {"min", [] { return make_unique<StatsAggregators::Min>(); }},
+    {"max", [] { return make_unique<StatsAggregators::Max>(); }},
+    {"avg", [] { return make_unique<StatsAggregators::Average>(); }},
+    {"mode", [] { return make_unique<StatsAggregators::Mode>(); }}
+  };
 
-unique_ptr<StatsAggregator> ReadAggregators(istream& input) 
-{
-	using namespace StatsAggregators;
+  auto result = make_unique<StatsAggregators::Composite>();
 
-	const unordered_map<string, std::function<unique_ptr<StatsAggregator>()>> known_builders = {
-	    {"sum", [] { return make_unique<Sum>(); }},
-	    {"min", [] { return make_unique<Min>(); }},
-	    {"max", [] { return make_unique<Max>(); }},
-	    {"avg", [] { return make_unique<Average>(); }},
-	    {"mode", [] { return make_unique<Mode>(); }}
-	};
+  int aggr_count;
+  input >> aggr_count;
 
-	auto result = make_unique<Composite>();
+  string line;
+  for (int i = 0; i < aggr_count; ++i) {
+    input >> line;
+    result->Add(known_builders.at(line)());
+  }
 
-	int aggr_count;
-	input >> aggr_count;
-
-	string line;
-	for (int i = 0; i < aggr_count; ++i)
-	{
-		input >> line;
-		result->Add(known_builders.at(line)());
-	}
-
-	return result;
+  return result;
 }
 
+int main() {
+  TestAll();
 
-int main()
-{
-	TestAll();
+  auto stats_aggregator = ReadAggregators(cin);
 
-	auto stats_aggregator = ReadAggregators(cin);
+  for (int value; cin >> value; ) {
+    stats_aggregator->Process(value);
+  }
+  stats_aggregator->PrintValue(cout);
 
-	for (int value; cin >> value; )
-	{
-		stats_aggregator->Process(value);
-	}
-
-	stats_aggregator->PrintValue(cout);
-
-	return 0;
+  return 0;
 }
 
-
-void TestAll()
-{
-	using namespace StatsAggregators;
-
-	TestRunner tr;
-	RUN_TEST(tr, TestSum);
-	RUN_TEST(tr, TestMin);
-	RUN_TEST(tr, TestMax);
-	RUN_TEST(tr, TestAverage);
-	RUN_TEST(tr, TestMode);
-	RUN_TEST(tr, TestComposite);
+void TestAll() {
+  TestRunner tr;
+  RUN_TEST(tr, StatsAggregators::TestSum);
+  RUN_TEST(tr, StatsAggregators::TestMin);
+  RUN_TEST(tr, StatsAggregators::TestMax);
+  RUN_TEST(tr, StatsAggregators::TestAverage);
+  RUN_TEST(tr, StatsAggregators::TestMode);
+  RUN_TEST(tr, StatsAggregators::TestComposite);
 }
