a4905cbac0b6ce64af99d8ed0e60385cf2d6573f
Hrodvintir <33006079+Hrodvintir@users.noreply.github.com>
2022-01-19 11:27:24 +0200

    Update solution.cpp

--- week3/07 Practice Assignment/solution.cpp
+++ week3/07 Practice Assignment/solution.cpp
@@ -9,25 +9,25 @@
 using Zoo = std::vector<std::unique_ptr<Animal>>;
 
 Zoo CreateZoo(std::istream& in) {
-  using namespace std;
-  Zoo zoo;
-  string word;
-  while (in >> word) {
-    if (word == "Tiger") {
-      zoo.push_back(make_unique<Tiger>());
-    } else if (word == "Wolf") {
-      zoo.push_back(make_unique<Wolf>());
-    } else if (word == "Fox") {
-      zoo.push_back(make_unique<Fox>());
-    } else {
-      throw runtime_error("Unknown animal!");
+    using namespace std;
+    Zoo zoo;
+    string word;
+    while (in >> word) {
+        if (word == "Tiger") {
+            zoo.push_back(make_unique<Tiger>());
+        } else if (word == "Wolf") {
+            zoo.push_back(make_unique<Wolf>());
+        } else if (word == "Fox") {
+            zoo.push_back(make_unique<Fox>());
+        } else {
+            throw runtime_error("Unknown animal!");
+        }
     }
-  }
-  return zoo;
+    return zoo;
 }
 
 void Process(const Zoo& zoo, std::ostream& out) {
-  for (const auto& animal : zoo) {
-    out << animal->Voice() << "\n";
-  }
+    for (const auto& animal : zoo) {
+        out << animal->Voice() << "\n";
+    }
 }
