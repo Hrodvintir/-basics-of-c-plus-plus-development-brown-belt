c13ed831e17a26cd79aedb7f843254269a8b3a98
Hrodvintir <33006079+Hrodvintir@users.noreply.github.com>
2022-01-19 14:24:07 +0200

    Update solution.cpp

--- week1/05 Programming Assignment/solution.cpp
+++ week1/05 Programming Assignment/solution.cpp
@@ -1,124 +1,77 @@
 #include <limits>
 #include <random>
 #include <unordered_set>
-#include <vector>
-#include "hash_person.h"
-#include "UnitTestsFramework.h"
 
+using namespace std;
 
+struct Address {
+  string city, street;
+  int building;
 
-// $ sort -R /usr/share/dict/propernames | head -n 100
-//
-// http://www.freebsd.org/cgi/cvsweb.cgi/~checkout~/src/share/dict/propernames
-
-const std::vector<std::string> WORDS = 
-{
-  "Kieran", "Jong", "Jisheng", "Vickie", "Adam", "Simon", "Lance",
-  "Everett", "Bryan", "Timothy", "Daren", "Emmett", "Edwin", "List",
-  "Sharon", "Trying", "Dan", "Saad", "Kamiya", "Nikolai", "Del",
-  "Casper", "Arthur", "Mac", "Rajesh", "Belinda", "Robin", "Lenora",
-  "Carisa", "Penny", "Sabrina", "Ofer", "Suzanne", "Pria", "Magnus",
-  "Ralph", "Cathrin", "Phill", "Alex", "Reinhard", "Marsh", "Tandy",
-  "Mongo", "Matthieu", "Sundaresan", "Piotr", "Ramneek", "Lynne", "Erwin",
-  "Edgar", "Srikanth", "Kimberly", "Jingbai", "Lui", "Jussi", "Wilmer",
-  "Stuart", "Grant", "Hotta", "Stan", "Samir", "Ramadoss", "Narendra",
-  "Gill", "Jeff", "Raul", "Ken", "Rahul", "Max", "Agatha",
-  "Elizabeth", "Tai", "Ellen", "Matt", "Ian", "Toerless", "Naomi",
-  "Rodent", "Terrance", "Ethan", "Florian", "Rik", "Stanislaw", "Mott",
-  "Charlie", "Marguerite", "Hitoshi", "Panacea", "Dieter", "Randell", "Earle",
-  "Rajiv", "Ted", "Mann", "Bobbie", "Pat", "Olivier", "Harmon",
-  "Raman", "Justin"
+  bool operator==(const Address& other) const {
+    return
+        city == other.city &&
+            street == other.street &&
+            building == other.building;
+  }
 };
 
+struct Person {
+  string name;
+  int height;
+  double weight;
+  Address address;
 
-void _TestSmoke() 
-{
-    std::vector<Person> points = 
-	{
-	  {"John", 180, 82.5, Address{"London", "Baker St", 221}},
-	  {"Sherlock", 190, 75.3, Address{"London", "Baker St", 221}},
-	};
-
-    std::unordered_set<Person, PersonHasher> point_set;
-	for (const auto& point : points) 
-	{
-		point_set.insert(point);
-	}
-
-	ASSERT_EQUAL(points.size(), point_set.size());
-	for (const auto& point : points) 
-	{
-		ASSERT_EQUAL(point_set.count(point), static_cast<size_t>(1));
-	}
-}
-
-
-void TestPurity_() 
-{
-	Person person = { "John", 180, 82.5, {"London", "Baker St", 221} };
-	PersonHasher hasher;
-
-	auto hash = hasher(person);
-	for (size_t t = 0; t < 100; ++t) 
-	{
-		ASSERT_EQUAL(hasher(person), hash);
-	}
+  bool operator==(const Person& other) const {
+    return
+        name == other.name &&
+            height == other.height &&
+            weight == other.weight &&
+            address == other.address;
+  }
 };
 
+struct AddressHasher {
+  size_t operator()(const Address& address) const {
+    // выбираем в качестве коэффициента довольно большое простое число;
+    // свободный член можем положить равным нулю, т.к. он не влияет
+    // на коллизии, а лишь циклически смещает бакеты
+    const size_t coef = 514'229;
 
-void TestDistribution_() 
-{
-	auto seed = 42;
-    std::mt19937 gen(seed);
+    // std::hash обычно не имеет состояния, поэтому создание
+    // локальных переменных не сопряжено с накладными расходами,
+    // а наоборот, помогает компилятору лучше оптимизировать код
+    const hash<int> int_hasher;
+    const hash<string> string_hasher;
 
-    std::uniform_int_distribution<int> height_dist(150, 200);
-    std::uniform_int_distribution<int> weight_dist(100, 240);  // [50, 120]
-    std::uniform_int_distribution<int> building_dist(1, 300);
-    std::uniform_int_distribution<int> word_dist(0, WORDS.size() - 1);
+    return (
+        coef * coef * string_hasher(address.city) +
+            coef * string_hasher(address.street) +
+            int_hasher(address.building)
+    );
+  }
+};
 
-	PersonHasher hasher;
+struct PersonHasher {
+  size_t operator()(const Person& person) const {
+    // выбираем в качестве коэффициента довольно большое простое число;
+    // свободный член можем положить равным нулю, т.к. он не влияет
+    // на коллизии, а лишь циклически смещает бакеты
+    const size_t coef = 39'916'801;
 
+    // std::hash обычно не имеет состояния, поэтому создание
+    // локальных переменных не сопряжено с накладными расходами,
+    // а наоборот, помогает компилятору лучше оптимизировать код
+    const hash<int> int_hasher;
+    const hash<double> double_hasher;
+    const hash<string> string_hasher;
+    const AddressHasher address_hasher;
 
-	const size_t num_buckets = 2053;
-
-	
-	const size_t perfect_bucket_size = 50;
-	const size_t num_points = num_buckets * perfect_bucket_size;
-    std::vector<size_t> buckets(num_buckets);
-	
-    for (size_t t = 0; t < num_points; ++t) 
-	{
-		Person person;
-		person.name = WORDS[word_dist(gen)];
-		person.height = height_dist(gen);
-		person.weight = weight_dist(gen) * 0.5;
-		person.address.city = WORDS[word_dist(gen)];
-		person.address.street = WORDS[word_dist(gen)];
-		person.address.building = building_dist(gen);
-		++buckets[hasher(person) % num_buckets];
-	}
-
-	
-	double pearson_stat = 0;
-	
-    for (auto bucket_size : buckets) 
-	{
-		size_t size_diff = bucket_size - perfect_bucket_size;
-		pearson_stat +=
-			size_diff * size_diff / static_cast<double>(perfect_bucket_size);
-	}
-
-	// >>> scipy.stats.chi2.ppf(0.95, 2052)
-	
-    const double critical_value = 2158.4981036918693;
-	ASSERT(pearson_stat < critical_value);
-}
-
-
-void TestHashPerson() 
-{
-	TestRunner tr;
-	RUN_TEST(tr, _TestSmoke);
-    RUN_TEST(tr, TestPurity_);
-	RUN_TEST(tr, TestDistribution_);
-}
+    return (
+        coef * coef * coef * string_hasher(person.name) +
+            coef * coef * int_hasher(person.height) +
+            coef * double_hasher(person.weight) +
+            address_hasher(person.address)
+    );
+  }
+};
