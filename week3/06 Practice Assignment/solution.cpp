c05bab47ebc5232d8ffe4f02aaf295c31a144ffe
Hrodvintir <33006079+Hrodvintir@users.noreply.github.com>
2022-01-19 11:29:59 +0200

    Update solution.cpp

--- week3/06 Practice Assignment/solution.cpp
+++ week3/06 Practice Assignment/solution.cpp
@@ -10,54 +10,54 @@ using namespace std;
 template <class T>
 class ObjectPool {
 public:
-  T* Allocate();
-  T* TryAllocate();
+    T* Allocate();
+    T* TryAllocate();
 
-  void Deallocate(T* object);
+    void Deallocate(T* object);
 
-  ~ObjectPool();
+    ~ObjectPool();
 
 private:
-  queue<T*> free;
-  set<T*> allocated;
+    queue<T*> free;
+    set<T*> allocated;
 };
 
 template <typename T>
 T* ObjectPool<T>::Allocate() {
-  if (free.empty()) {
-    free.push(new T);
-  }
-  auto ret = free.front();
-  free.pop();
-  allocated.insert(ret);
-  return ret;
+    if (free.empty()) {
+        free.push(new T);
+    }
+    auto ret = free.front();
+    free.pop();
+    allocated.insert(ret);
+    return ret;
 }
 
 template <typename T>
 T* ObjectPool<T>::TryAllocate() {
-  if (free.empty()) {
-    return nullptr;
-  }
-  return Allocate();
+    if (free.empty()) {
+        return nullptr;
+    }
+    return Allocate();
 }
 
 template <typename T>
 void ObjectPool<T>::Deallocate(T* object) {
-  if (allocated.find(object) == allocated.end()) {
-    throw invalid_argument("");
-  }
-  allocated.erase(object);
-  free.push(object);
+    if (allocated.find(object) == allocated.end()) {
+        throw invalid_argument("");
+    }
+    allocated.erase(object);
+    free.push(object);
 }
 
 template <typename T>
 ObjectPool<T>::~ObjectPool() {
-  for (auto x : allocated) {
-    delete x;
-  }
-  while (!free.empty()) {
-    auto x = free.front();
-    free.pop();
-    delete x;
-  }
+    for (auto x : allocated) {
+        delete x;
+    }
+    while (!free.empty()) {
+        auto x = free.front();
+        free.pop();
+        delete x;
+    }
 }
