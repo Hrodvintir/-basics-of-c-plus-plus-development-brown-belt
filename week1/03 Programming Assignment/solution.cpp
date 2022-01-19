bb6182d8113d30f1347999ee62c9fa1159ffe57c
Hrodvintir <33006079+Hrodvintir@users.noreply.github.com>
2022-01-19 12:22:22 +0200

    Update solution.cpp

--- week1/03 Programming Assignment/solution.cpp
+++ week1/03 Programming Assignment/solution.cpp
@@ -1,54 +1,39 @@
-#include "UnitTestsFramework.h"
-#include "set_iterator_next.h"
+#include "test_runner.h"
 
+struct Node {
+  Node(int v, Node* p)
+      : value(v)
+      , parent(p)
+  {}
 
-void Test1() 
-{
-	NodeBuilder nb;
+  int value;
+  Node* left = nullptr;
+  Node* right = nullptr;
+  Node* parent;
+};
 
-	Node* root = nb.CreateRoot(50);
-	ASSERT_EQUAL(root->value, 50);
-
-	Node* l = nb.CreateLeftSon(root, 2);
-	Node* min = nb.CreateLeftSon(l, 1);
-	Node* r = nb.CreateRightSon(l, 4);
-	ASSERT_EQUAL(min->value, 1);
-	ASSERT_EQUAL(r->parent->value, 2);
-
-	nb.CreateLeftSon(r, 3);
-	nb.CreateRightSon(r, 5);
-
-	r = nb.CreateRightSon(root, 100);
-	l = nb.CreateLeftSon(r, 90);
-	nb.CreateRightSon(r, 101);
-
-	nb.CreateLeftSon(l, 89);
-	r = nb.CreateRightSon(l, 91);
-
-	ASSERT_EQUAL(Next(l)->value, 91);
-	ASSERT_EQUAL(Next(root)->value, 89);
-	ASSERT_EQUAL(Next(min)->value, 2);
-	ASSERT_EQUAL(Next(r)->value, 100);
-
-	while (min) 
-	{
-        std::cout << min->value << '\n';
-		min = Next(min);
-	}
+Node* GetLeftest(Node* me) {
+  assert(me);
+  while (me->left) {
+    me = me->left;
+  }
+  return me;
 }
 
 
-void TestRootOnly() 
-{
-	NodeBuilder nb;
-	Node* root = nb.CreateRoot(42);
-	ASSERT(Next(root) == nullptr);
-};
+Node* Next(Node* me) {
+  assert(me);
 
+  if (me->right) {
+    return GetLeftest(me->right);
+  }
 
-void TestSetIteratorNext() 
-{
-	TestRunner tr;
-	RUN_TEST(tr, Test1);
-	RUN_TEST(tr, TestRootOnly);
+  if (me->parent && me->parent->left == me) {
+    return me->parent;
+  }
+
+  while (me->parent && me == me->parent->right) {
+    me = me->parent;
+  }
+  return me->parent;
 }
