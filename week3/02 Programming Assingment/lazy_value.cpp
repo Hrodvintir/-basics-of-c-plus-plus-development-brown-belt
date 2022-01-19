dd87d31ba81352451fff56b819707b8ebf26476a
Hrodvintir <33006079+Hrodvintir@users.noreply.github.com>
2022-01-19 11:35:08 +0200

    Update lazy_value.cpp

--- week3/02 Programming Assingment/lazy_value.cpp
+++ week3/02 Programming Assingment/lazy_value.cpp
@@ -2,45 +2,58 @@
 
 #include <functional>
 #include <string>
+#include <optional>
+
 using namespace std;
 
-template <typename T>
+template<typename T>
 class LazyValue {
 public:
-  explicit LazyValue(std::function<T()> init);
+    explicit LazyValue(std::function<T()> init) : init_(std::move(init)) {
 
-  bool HasValue() const;
-  const T& Get() const;
+    }
+
+    bool HasValue() const {
+        return value_.has_value();
+    }
+
+    const T &Get() const {
+        if (!value_) {
+            value_ = init_();
+        }
+        return *value_;
+    }
 
 private:
-
+    std::function<T()> init_;
+    mutable std::optional<T> value_;
 };
 
 void UseExample() {
-  const string big_string = "Giant amounts of memory";
+    const string big_string = "Giant amounts of memory";
 
-  LazyValue<string> lazy_string([&big_string] { return big_string; });
+    LazyValue<string> lazy_string([&big_string] { return big_string; });
 
-  ASSERT(!lazy_string.HasValue());
-  ASSERT_EQUAL(lazy_string.Get(), big_string);
-  ASSERT_EQUAL(lazy_string.Get(), big_string);
+    ASSERT(!lazy_string.HasValue());
+    ASSERT_EQUAL(lazy_string.Get(), big_string);
+    ASSERT_EQUAL(lazy_string.Get(), big_string);
 }
 
 void TestInitializerIsntCalled() {
-  bool called = false;
+    bool called = false;
 
-  {
-    LazyValue<int> lazy_int([&called] {
-      called = true;
-      return 0;
-    });
-  }
-  ASSERT(!called);
+    {
+        LazyValue<int> lazy_int([&called] {
+            called = true;
+            return 0;
+        });
+    }
+    ASSERT(!called);
 }
 
 int main() {
-  TestRunner tr;
-  RUN_TEST(tr, UseExample);
-  RUN_TEST(tr, TestInitializerIsntCalled);
-  return 0;
+    TestRunner tr;
+    RUN_TEST(tr, UseExample);
+    RUN_TEST(tr, TestInitializerIsntCalled);
+    return 0;
 }
