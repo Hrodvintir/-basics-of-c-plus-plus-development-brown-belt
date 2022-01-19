d861ea4b0644c0a221ae4da219c8d98b0618c522
Hrodvintir <33006079+Hrodvintir@users.noreply.github.com>
2022-01-19 12:18:14 +0200

    Update solution.cpp

--- week1/02 Programming Assingment/Solution/solution.cpp
+++ week1/02 Programming Assingment/Solution/solution.cpp
@@ -1,81 +1,52 @@
-#include "UnitTestsFramework.h"
-#include "hash_set.h"
+#include <algorithm>
+#include <forward_list>
 #include <iterator>
+#include <vector>
 
+using namespace std;
 
-void TestSmoke()
-{
-	HashSet<int, IntHasher> hash_set(2);
-	hash_set.Add(3);
-	hash_set.Add(4);
+template <typename Type, typename Hasher = hash<Type>>
+class HashSet {
+ public:
+  using BucketList = forward_list<Type>;
 
-	ASSERT(hash_set.Has(3));
-	ASSERT(hash_set.Has(4));
-	ASSERT(!hash_set.Has(5));
+ public:
+  explicit HashSet(
+      size_t num_buckets,
+      const Hasher& hasher = {}
+  )
+      : hasher_(hasher)
+      , buckets_(num_buckets)
+  {}
 
-	hash_set.Erase(3);
+  void Add(const Type& value) {
+    auto& bucket = buckets_[GetBucketIndex(value)];
+    auto it = find(begin(bucket), end(bucket), value);
 
-	ASSERT(!hash_set.Has(3));
-	ASSERT(hash_set.Has(4));
-	ASSERT(!hash_set.Has(5));
+    if (it == bucket.end()) {
+      bucket.push_front(value);
+    }
+  }
 
-	hash_set.Add(3);
-	hash_set.Add(5);
+  bool Has(const Type& value) const {
+    const auto& bucket = GetBucket(value);
+    return find(begin(bucket), end(bucket), value) != bucket.end();
+  }
 
-	ASSERT(hash_set.Has(3));
-	ASSERT(hash_set.Has(4));
-	ASSERT(hash_set.Has(5));
-}
+  void Erase(const Type& value) {
+    buckets_[GetBucketIndex(value)].remove(value);
+  }
 
+  const BucketList& GetBucket(const Type& value) const {
+    return buckets_[GetBucketIndex(value)];
+  }
 
-void TestEmpty() 
-{
-	const HashSet<int, IntHasher> hash_set(10);
-	
-    for (int value = 0; value < 10000; ++value) 
-	{
-		ASSERT(!hash_set.Has(value));
-	}
-}
+ private:
+  size_t GetBucketIndex(const Type& value) const {
+    return hasher_(value) % buckets_.size();
+  }
 
-
-void TestIdempotency()
-{
-	HashSet<int, IntHasher> hash_set(10);
-	hash_set.Add(5);
-	ASSERT(hash_set.Has(5));
-	hash_set.Add(5);
-	ASSERT(hash_set.Has(5));
-	hash_set.Erase(5);
-	ASSERT(!hash_set.Has(5));
-	hash_set.Erase(5);
-	ASSERT(!hash_set.Has(5));
-}
-
-
-void TestEquivalence()
-{
-	HashSet<TestValue, TestValueHasher> hash_set(10);
-	hash_set.Add(TestValue{ 2 });
-	hash_set.Add(TestValue{ 3 });
-
-	ASSERT(hash_set.Has(TestValue{ 2 }));
-	ASSERT(hash_set.Has(TestValue{ 3 }));
-
-	const auto& bucket = hash_set.GetBucket(TestValue{ 2 });
-	const auto& three_bucket = hash_set.GetBucket(TestValue{ 3 });
-	ASSERT_EQUAL(&bucket, &three_bucket);
-
-	ASSERT_EQUAL(1, std::distance(std::begin(bucket), std::end(bucket)));
-	ASSERT_EQUAL(2, bucket.front().value);
-}
-
-
-void TestHashSet() 
-{
-	TestRunner tr;
-	RUN_TEST(tr, TestSmoke);
-	RUN_TEST(tr, TestEmpty);
-	RUN_TEST(tr, TestIdempotency);
-	RUN_TEST(tr, TestEquivalence);
-}
+ private:
+  Hasher hasher_;
+  vector<BucketList> buckets_;
+};
