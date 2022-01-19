448fc492f6e222cd079fd0e957a4e2144e8ac210
Hrodvintir <33006079+Hrodvintir@users.noreply.github.com>
2022-01-19 21:12:50 +0200

    Update solution.cpp

--- week2/03 Programming Assingment/solution.cpp
+++ week2/03 Programming Assingment/solution.cpp
@@ -1,69 +1,35 @@
 #include "ini.h"
-#include <utility>
 
+namespace Ini {
 
-namespace Ini
-{
-    Section& Document::AddSection(std::string name)
-    {
-		return sections[std::move(name)];
-    }
+    Document Load(istream &input) {
+        Document result;
 
-
-    const Section& Document::GetSection(const std::string& name) const
-    {
-		return sections.at(name);
-    }
-
-
-    size_t Document::SectionCount() const
-    {
-		return sections.size();
-    }
-
-
-    std::string_view ParseHeader(std::string_view line)
-    {
-		size_t pos = line.find_first_of('[');
-		line.remove_prefix(pos + 1);
-
-		pos = line.find_first_of(']');
-
-		return line.substr(0, pos);
-    }
-
-
-    std::pair<std::string, std::string> ParseExpression(std::string_view line)
-    {
-		const size_t pos = line.find_first_of('=');
-		const std::string_view first = line.substr(0, pos);
-
-		line.remove_prefix(pos + 1);
-
-		return std::make_pair(std::string(first), std::string(line));
-    }
-
-
-    Document Load(std::istream& input)
-    {
-		Document document;
-		Section* section = nullptr;
-
-        for (std::string line; std::getline(input, line);)
-        {
-            if (!line .empty())
-            {
-                if (line[0] == '[')
-                {
-					section = &document.AddSection(std::string(ParseHeader(line)));
-                }
-                else
-                {
-					section->insert(ParseExpression(line));
+        Section *current_section = nullptr;
+        for (string line; getline(input, line);) {
+            if (!line.empty()) {
+                if (line[0] == '[') {
+                    current_section = &result.AddSection(line.substr(1, line.size() - 2));
+                } else {
+                    size_t eq_pos = line.find('=');
+                    current_section->insert({line.substr(0, eq_pos), line.substr(eq_pos + 1)});
                 }
             }
         }
 
-		return document;
+        return result;
     }
-}
+
+    Section &Document::AddSection(string name) {
+        return sections[name];
+    }
+
+    const Section &Document::GetSection(const string &name) const {
+        return sections.at(name);
+    }
+
+    size_t Document::SectionCount() const {
+        return sections.size();
+    }
+
+} /* namespace Ini */
